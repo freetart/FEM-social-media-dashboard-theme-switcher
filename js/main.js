9241f19f7f6657c40b33c78bd908bf394970607b
Chris M <habibdev96@gmail.com>
2021-05-25 02:29:00 -0600

    created dark theme toggle functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const changeTheme = () => {
+  const checkbox = document.getElementById("theme-toggle__checkbox");
+
+  checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+  });
+};
+changeTheme();
